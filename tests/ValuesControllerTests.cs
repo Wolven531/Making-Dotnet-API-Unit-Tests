88fbabda574070b2f41c5ed298eac670fb97cab0
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-21 15:58:46 -0700

    Fix test setup for simple controller, which now requires simple repository

--- tests/ValuesControllerTests.cs
+++ tests/ValuesControllerTests.cs
@@ -1,6 +1,8 @@
 using FluentAssertions;
 using making_unit_tests.Controllers;
+using making_unit_tests.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using Xunit;
 
@@ -11,11 +13,16 @@ namespace tests
 		[Fact]
 		public void Get_WhenInvoked_ShouldReturnArrayOfStrings()
 		{
-			var fixture = new ValuesController();
+			// Arrange
+			var mockEntityRepository = new Mock<IEntityRepository>();
+			var fixture = new ValuesController(mockEntityRepository.Object);
 
+			// Act
 			var actual = fixture.Get();
 
+			// Assert
 			actual.Value.Should().BeEquivalentTo(new[] { "value1", "value2" });
+			mockEntityRepository.VerifyNoOtherCalls();
 		}
 	}
 }
